01ab3ee8ae5c66db13340b9c7cd7bf04cd5f807a
Maria_Veremey <Maria_Veremey@epam.com>
2021-08-10 13:13:29 +0300

    reimplement tabs

--- js/custom.js
+++ js/custom.js
@@ -7,35 +7,35 @@
   });
 })();
 
-document.querySelectorAll("#nav li").forEach(function(navEl) {
-  navEl.onclick = function() {
-    toggleTab(this.id, this.dataset.target);
-  };
-});
-
-function toggleTab(selectedNav, targetId) {
-  var navEls = document.querySelectorAll("#nav li");
-
-  navEls.forEach(function(navEl) {
-    if (navEl.id == selectedNav) {
-      navEl.classList.add("is-active");
-    } else {
-      if (navEl.classList.contains("is-active")) {
-        navEl.classList.remove("is-active");
-      }
-    }
-  });
-
-  var tabs = document.querySelectorAll(".tab-pane");
-
-  tabs.forEach(function(tab) {
-    if (tab.id == targetId) {
-      tab.style.display = "block";
-    } else {
-      tab.style.display = "none";
-    }
-  });
-}
+// document.querySelectorAll("#nav .button").forEach(function(navEl) {
+//   navEl.onclick = function() {
+//     toggleTab(this.id, this.dataset.target);
+//   };
+// });
+//
+// function toggleTab(selectedNav, targetId) {
+//   var navEls = document.querySelectorAll("#nav .button");
+//
+//   navEls.forEach(function(navEl) {
+//     if (navEl.id == selectedNav) {
+//       navEl.classList.add("is-active");
+//     } else {
+//       if (navEl.classList.contains("is-active")) {
+//         navEl.classList.remove("is-active");
+//       }
+//     }
+//   });
+//
+//   var tabs = document.querySelectorAll(".tab-pane");
+//
+//   tabs.forEach(function(tab) {
+//     if (tab.id == targetId) {
+//       tab.style.display = "block";
+//     } else {
+//       tab.style.display = "none";
+//     }
+//   });
+// }
 
 document.querySelector('.skip-button').addEventListener('click', (e)=>{
   const skipMenu = document.querySelector('.skipMenu-menu');
@@ -56,3 +56,134 @@ document.querySelector('.skip-button').addEventListener('click', (e)=>{
 document.querySelector('.skipMenu-menu').addEventListener('click', (e)=>{
   e.currentTarget.classList.toggle('open');
 });
+
+
+
+(function () {
+  const tabs = document.querySelectorAll('[role="tab"]');
+  const panels = document.querySelectorAll('[role="tabpanel"]')
+  const delay = 200;
+
+  const keys = {
+    end: 35,
+    home: 36,
+    left: 37,
+    up: 38,
+    right: 39,
+    down: 40,
+  };
+
+  const direction = {
+    37: -1,
+    38: -1,
+    39: 1,
+    40: 1
+  };
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', clickEventListener);
+    tab.addEventListener('keydown', keydownEventListener);
+    tab.addEventListener('keyup', keyupEventListener);
+
+    tab.index = index;
+  })
+
+  function clickEventListener (event) {
+    const tab = event.currentTarget;
+    activateTab(tab, false);
+  }
+
+  function keydownEventListener (event) {
+    const key = event.keyCode;
+
+    switch (key) {
+      case keys.end:
+        event.preventDefault();
+        activateTab(tabs[tabs.length - 1]);
+        break;
+      case keys.home:
+        event.preventDefault();
+        activateTab(tabs[0]);
+        break;
+    }
+  }
+
+  function keyupEventListener (event) {
+    const key = event.keyCode;
+
+    if (key === keys.left || key === keys.right) {
+      switchTabOnArrowPress(event);
+    }
+  }
+
+  function switchTabOnArrowPress (event) {
+    const pressed = event.keyCode;
+
+    tabs.forEach(tab => {
+      tab.addEventListener('focus', focusEventHandler);
+    })
+
+    if (direction[pressed]) {
+      const target = event.currentTarget;
+      if (target.index !== undefined) {
+        if (tabs[target.index + direction[pressed]]) {
+          tabs[target.index + direction[pressed]].focus();
+        }
+        else if (pressed === keys.left || pressed === keys.up) {
+          focusLastTab();
+        }
+        else if (pressed === keys.right || pressed === keys.down) {
+          focusFirstTab();
+        }
+      }
+    }
+  }
+
+  function activateTab (tab, setFocus) {
+    setFocus = setFocus || true;
+    deactivateTabs();
+
+    tab.removeAttribute('tabindex');
+    tab.setAttribute('aria-selected', 'true');
+    tab.classList.add('is-active');
+
+    const controls = tab.getAttribute('aria-controls');
+    document.getElementById(controls).classList.add('is-active');
+    if (setFocus) {
+      tab.focus();
+    }
+  }
+
+  function deactivateTabs () {
+    tabs.forEach(tab => {
+      tab.setAttribute('tabindex', '-1');
+      tab.setAttribute('aria-selected', 'false');
+      tab.removeEventListener('focus', focusEventHandler);
+      tab.classList.remove('is-active');
+    })
+
+    panels.forEach((panel)=> panel.classList.remove('is-active'));
+  }
+
+  function focusFirstTab () {
+    tabs[0].focus();
+  }
+
+  function focusLastTab () {
+    tabs[tabs.length - 1].focus();
+  }
+
+  function focusEventHandler (event) {
+    const target = event.target;
+
+    setTimeout(checkTabFocus, delay, target);
+  }
+
+  function checkTabFocus (target) {
+    let focused = document.activeElement;
+
+    if (target === focused) {
+      activateTab(target, false);
+    }
+  }
+}());
